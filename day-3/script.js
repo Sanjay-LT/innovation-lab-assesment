d73aa5057ac3027443fae66fb86d7b14e7892418
Sanjay-LT <ltsanjay69@gmail.com>
2025-07-27 22:33:38 +0530

    Add files via upload

---
+++ day-3/script.js
@@ -0,0 +1,27 @@
+var inputbox = parseInt(document.getElementById("input-box1").value)
+var inputbox2 = parseInt(document.getElementById("input-box2").value)
+
+function add1(){
+    var inputbox = parseInt(document.getElementById("input-box1").value)
+var inputbox2 = parseInt(document.getElementById("input-box2").value)
+    console.log(inputbox + inputbox2)
+    
+}
+function sub1(){
+    var inputbox = parseInt(document.getElementById("input-box1").value)
+var inputbox2 = parseInt(document.getElementById("input-box2").value)
+    console.log(inputbox  - inputbox2)
+    
+}
+function mul(){
+    var inputbox = parseInt(document.getElementById("input-box1").value)
+var inputbox2 = parseInt(document.getElementById("input-box2").value)
+    console.log(inputbox * inputbox2)
+    
+}
+function divi(){
+    var inputbox = parseInt(document.getElementById("input-box1").value)
+var inputbox2 = parseInt(document.getElementById("input-box2").value)
+    console.log(inputbox / inputbox2)
+    
+}
